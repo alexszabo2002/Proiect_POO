4a3bb6fb0baa88d982cff67bef395346b0fad0c1
Alex Szabo <szaboalexandru2013@gmail.com>
2022-12-16 22:16:00 +0200

    Baza pt clasele Eveniment si Zona

---
+++ Zona.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <string>
+#include "Zona.h"
+
+using namespace std;
+
+Zona::Zona()
+{
+	denumireZona = "N/A";
+	nrLocuriZona = 0;
+	locuri = nullptr;
+}
+
+Zona::Zona(string _denumireZona, int _nrLocuriZona)
+{
+	denumireZona = _denumireZona;
+	nrLocuriZona = _nrLocuriZona;
+
+	if (nrLocuriZona > 0)
+	{
+		locuri = new int[_nrLocuriZona];
+
+		for (int i = 0; i < _nrLocuriZona; i++)
+		{
+			locuri[i] = i + 1;
+		}
+	}
+	else
+	{
+		nrLocuriZona = 0;
+		locuri = nullptr;
+	}
+}
+
+Zona::Zona(const Zona& z)
+{
+	this->denumireZona = z.denumireZona;
+
+	if (z.locuri != nullptr && z.nrLocuriZona > 0)
+	{
+		this->nrLocuriZona = z.nrLocuriZona;
+
+		this->locuri = new int[z.nrLocuriZona];
+		for (int i = 0; i < z.nrLocuriZona; i++)
+		{
+			this->locuri[i] = z.locuri[i];
+		}
+	}
+	else
+	{
+		this->nrLocuriZona = 0;
+		this->locuri = nullptr;
+	}
+}
+
+Zona::~Zona()
+{
+	if (locuri != nullptr && nrLocuriZona > 0)
+	{
+		delete[] locuri;
+		locuri = nullptr;
+	}
+}
+
+Zona& Zona::operator=(const Zona& z)
+{
+	if (this != &z)
+	{
+		this->denumireZona = z.denumireZona;
+
+		if (this->locuri != nullptr && this->nrLocuriZona > 0)
+		{
+			delete[] this->locuri;
+
+			this->nrLocuriZona = z.nrLocuriZona;
+
+			this->locuri = new int[z.nrLocuriZona];
+			for (int i = 0; i < z.nrLocuriZona; i++)
+			{
+				this->locuri[i] = z.locuri[i];
+			}
+		}
+		else
+		{
+			this->nrLocuriZona = 0;
+			this->locuri = nullptr;
+		}
+	}
+	return *this;
+}
+
+void Zona::setDenumireZona(string _denumireZona)
+{
+	this->denumireZona = _denumireZona;
+}
+
+string Zona::getDenumireZona()
+{
+	return denumireZona;
+}
+
+void Zona::setNrLocuriZona(int _nrLocuriZona)
+{
+	this->nrLocuriZona = _nrLocuriZona;
+}
+
+int Zona::getNrLocuriZona()
+{
+	return nrLocuriZona;
+}
+
+ostream& operator<<(ostream& out, Zona z)
+{
+	out << "Denumire zona: " << z.denumireZona << endl;
+	out << "Nr locuri zona: " << z.nrLocuriZona << endl;
+	out << endl;
+
+	for (int i = 0; i < z.nrLocuriZona; i++)
+	{
+		if (z.locuri[i] % 10 == 9)
+		{
+			out << z.locuri[i] << endl;
+		}
+		else
+		{
+			out << z.locuri[i] << " ";
+		}
+	}
+	out << endl;
+	return out;
+}
+
+istream& operator>>(istream& in, Zona& z)
+{
+	cout << "Denumire zona: ";
+	in >> ws;
+	getline(in, z.denumireZona);
+	cout << "Nr locuri zona: ";
+	in >> z.nrLocuriZona;
+
+	if (z.nrLocuriZona > 0)
+	{
+		z.locuri = new int[z.nrLocuriZona];
+
+		for (int i = 0; i < z.nrLocuriZona; i++)
+		{
+			z.locuri[i] = i + 1;
+		}
+	}
+
+	return in;
+}
--- Zona.h
+++ Zona.h
@@ -3,3 +3,27 @@
 #include <string>
 
 using namespace std;
+
+class Zona
+{
+private:
+	string denumireZona;
+	int nrLocuriZona;
+	int* locuri;
+
+public:
+	Zona();
+	Zona(string, int);
+	Zona(const Zona&);
+	~Zona();
+	Zona& operator=(const Zona&);
+
+	void setDenumireZona(string);
+	string getDenumireZona();
+
+	void setNrLocuriZona(int);
+	int getNrLocuriZona();
+
+	friend ostream& operator<<(ostream&, Zona);
+	friend istream& operator>>(istream&, Zona&);
+};
